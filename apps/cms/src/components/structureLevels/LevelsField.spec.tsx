6dfd064c7091fbe44a8c9211754079685cb37016
n3xtd0 <correo.solutionpc@gmail.com>
2025-12-07 18:53:58 +0000

    feat(cms): :sparkles: add testing-library dependencies for improved testing capabilities

--- apps/cms/src/components/structureLevels/LevelsField.spec.tsx
+++ apps/cms/src/components/structureLevels/LevelsField.spec.tsx
@@ -1,8 +1,8 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { LevelsField } from './LevelsField'
 
 describe('LevelsField', () => {
   it('should render', () => {
-    render(<LevelsField />)
+    render(<LevelsField field={{ type: 'relationship', name: 'levels', relationTo: 'level' }} path="levels" />)
   })
 })
