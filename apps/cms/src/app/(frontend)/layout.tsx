c4ba343efc56e8227451bc2f4aa18b09d4e48d51
n3xtd0 <correo.solutionpc@gmail.com>
2026-01-11 14:12:46 +0000

    feat(cms): :art: integrate Azeret Mono font into layout and FlipCard for improved typography

--- apps/cms/src/app/(frontend)/layout.tsx
+++ apps/cms/src/app/(frontend)/layout.tsx
@@ -2,18 +2,24 @@ import React from 'react'
 import './styles.css'
 import '@/styles/global.css'
 import { gothamCondensed } from './fonts'
+import { Azeret_Mono } from 'next/font/google';
 
 export const metadata = {
   description: 'Abacus Poker Club',
   title: 'Abacus Poker Club',
 }
 
+const azeretMono = Azeret_Mono({
+  subsets: ['latin'],
+  variable: '--font-azeret-mono',
+});
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function RootLayout(props: { children: any }) {
   const { children } = props
 
   return (
-    <html lang="en" className={gothamCondensed.variable}>
+    <html lang="en" className={`${gothamCondensed.variable} ${azeretMono.variable}`}>
       <body>
         <main>{children}</main>
       </body>
