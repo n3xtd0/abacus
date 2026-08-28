b1c0c4a6cd9c6c21bc3f29dc8592ecb6e15ce242
n3xtd0r <correo.solutionpc@gmail.com>
2026-08-28 17:17:28 +0100

    feat(cms): :sparkles: add rewrites for croupiers-clock and configure Vercel settings for optimized deployment

--- apps/cms/next.config.mjs
+++ apps/cms/next.config.mjs
@@ -4,6 +4,14 @@ import { withPayload } from '@payloadcms/next/withPayload'
 const nextConfig = {
   // Your Next.js config here
   turbopack: {},
+  async rewrites() {
+    return [
+      {
+        source: '/croupiers-clock/:path*',
+        destination: 'https://abacus-web-pi.vercel.app/:path*',
+      },
+    ];
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
